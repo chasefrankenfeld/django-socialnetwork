f345455412d594d091fb4c871d647b27a60e064b
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2017-08-30 17:06:51 -0400

    Added password authentication. Email will only be displayed in the console at this stage.

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login, password_reset
+from django.contrib.auth.views import (login, logout, logout_then_login,
+                                        password_change, password_change_done,
+                                        password_reset,password_reset_done,
+                                        password_reset_confirm, password_reset_complete)
 from . import views
 
 urlpatterns = [
@@ -9,5 +12,13 @@ urlpatterns = [
         url(r'^logout/$', logout, name='logout'),
         url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
 
+        # change password urls
+        url(r'^password-change/$', password_change, name='password_change'),
+        url(r'^password-change/done/$', password_change_done, name='password_change_done'),
+
+        # restore password urls
         url(r'^password_reset/$', password_reset, name='password_reset'),
-    ]
+        url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+        url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, name='password_reset_confirm'),
+        url(r'^reset/done/$', password_reset_complete, name='password_reset_complete'),
+        ]
