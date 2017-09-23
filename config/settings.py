ca82b4661ea39ca0d80506a5473decd40bdc3c6d
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2017-09-23 16:35:02 +1000

    created a posgres database

--- config/settings.py
+++ config/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookmarks',
+        'USER': 'chase',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
